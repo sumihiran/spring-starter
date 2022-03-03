9ee59667ff99b99210b869f810841fd6ba2f1bff
Nuwan Sumihiran <sumihiran.nuwan@gmail.com>
2022-03-03 09:16:30 +0530

    Pure spring boot in kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.6.2" // 22 Dec 2021
     id("io.spring.dependency-management") version "1.0.11.RELEASE" // 13 Jan 2021
-    id("com.vaadin") version "0.14.6.0"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.10" // December 14, 2021
     kotlin("plugin.jpa") version "1.6.10"
@@ -17,13 +16,11 @@ repositories {
     mavenCentral()
 }
 
-extra["vaadinVersion"] = "23.0.0"  // 02 Mar 2022
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.vaadin:vaadin-spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
@@ -32,7 +29,6 @@ dependencies {
 
 dependencyManagement {
     imports {
-        mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
     }
 }
 
